6eb5ec0235f4fcebcea7319f2ed36c9d81fa6e34
ElnurShukurov <shukurovs.e@gmail.com>
2026-01-03 17:35:06 +0400

    Dates

---
+++ src/main/java/com/demoqa/pages/widgets/DatePickerMenuPage.java
@@ -0,0 +1,38 @@
+package com.demoqa.pages.widgets;
+
+import org.openqa.selenium.By;
+
+import static utilities.DropDownUtility.selectByVisibleTest;
+
+public class DatePickerMenuPage extends WidgetsPage {
+
+    private By selectDateField = By.id("datePickerMonthYearInput");
+    private By monthDropDown = By.className("react-datepicker__month-select");
+    private By yearDropDown = By.cssSelector(".react-datepicker__year-select");
+
+    private By dayValue(String day) {
+        return By.xpath(
+                "//div[contains(@class, 'react-datepicker__day react-datepicker__day--')][text()='" + day + "']");
+    }
+
+    public void clickDay(String day) {
+        click(dayValue(day));
+    }
+
+    public void clickSelectDate() {
+        click(selectDateField);
+    }
+
+    public String getDate() {
+        return find(selectDateField).getAttribute("value");
+    }
+
+    public void selectMonth(String month) {
+        selectByVisibleTest(monthDropDown, month);
+    }
+
+    public void selectYear(String year) {
+        selectByVisibleTest(yearDropDown, year);
+    }
+
+}
