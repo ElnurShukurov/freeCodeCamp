3a6eff773162d29705e870a3b6174682376a0991
ElnurShukurov <shukurovs.e@gmail.com>
2026-01-06 06:42:48 +0400

    Confirmation and Prompt Alerts

--- src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/AlertsPage.java
@@ -7,9 +7,31 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 public class AlertsPage extends Alerts_Frames_WindowsPage {
 
     private By informationAlertButton = By.id("alertButton");
+    private By confirmationAlertButton = By.id("confirmButton");
+    private By confirmationResult = By.id("confirmResult");
+    private By promptAlertButton = By.id("promtButton");
+    private By promptResult = By.id("promptResult");
 
     public void clickInformationAlertButton() {
         scrollToElementJS(informationAlertButton);
         click(informationAlertButton);
+    }
+
+    public void clickConfirmationAlertButton() {
+        scrollToElementJS(confirmationAlertButton);
+        click(confirmationAlertButton);
+    }
+
+    public String getConfirmationResult() {
+        return find(confirmationResult).getText();
+    }
+
+    public void clickPromptAlertButton() {
+        scrollToElementJS(promptAlertButton);
+        click(promptAlertButton);
+    }
+
+    public String getPromptAlertResult() {
+        return find(promptResult).getText();
     }
 }
--- src/test/java/part3_4/com/demoqa/test/part4/alerts/AlertsTest.java
+++ src/test/java/part3_4/com/demoqa/test/part4/alerts/AlertsTest.java
@@ -6,9 +6,9 @@ import part3_4.com.demoqa.base.BaseTest;
 
 import static utilities.SwitchToUtility.*;
 
+@Test
 public class AlertsTest extends BaseTest {
 
-    @Test
     public void testInformationAlert() {
         String expectedAlertText = "You clicked a button";
         var alertsPage = homePage.gotoAlertsFramesWindowsCard().clickAlerts();
@@ -16,5 +16,28 @@ public class AlertsTest extends BaseTest {
         Assert.assertEquals(getAlertText(), expectedAlertText,
                 "\n Actual and Expected messages do not match \n");
         acceptAlert();
+    }
+
+    public void testConfirmationAlert() {
+        var alertsPage = homePage.gotoAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickConfirmationAlertButton();
+        dismissAlert();
+        String actualConfirmationResult = alertsPage.getConfirmationResult();
+        String expectedConfirmationResult = "You selected Cancel";
+        Assert.assertEquals(actualConfirmationResult, expectedConfirmationResult,
+                "\n You did not select Cancel \n");
+    }
+
+    public void testPromptAlert() {
+        String alertText = "Selenium with Java";
+        var alertsPage = homePage.gotoAlertsFramesWindowsCard().clickAlerts();
+        alertsPage.clickPromptAlertButton();
+        setAlertText(alertText);
+        acceptAlert();
+        String actualResult = alertsPage.getPromptAlertResult();
+        String expectedResult = "You entered " + alertText;
+        Assert.assertEquals(actualResult, expectedResult,
+                "\n Actual and Expected results do not match \n");
+
     }
 }
