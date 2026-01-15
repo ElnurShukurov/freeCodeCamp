4fde36926d1ba4accdf7f56aac96d905e6ba4444
ElnurShukurov <shukurovs.e@gmail.com>
2026-01-15 20:52:12 +0400

    Simulate Mouse Movements

--- src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -10,6 +10,7 @@ public class WidgetsPage extends HomePage {
     private By selectMenuItem = By.xpath("//li[@id='item-8']/span[text()='Select Menu']");
     private By datePickerMenuItem = By.xpath("//li[@id='item-2']/span[text()='Date Picker']");
     private By progressBarMenuItem = By.xpath("//li[@id='item-4']/span[text()='Progress Bar']");
+    private By sliderMenuItem = By.xpath("//li[@id='item-3']/span[text()='Slider']");
 
     public SelectMenuPage clickSelectMenu() {
         scrollToElementJS(selectMenuItem);
@@ -27,5 +28,11 @@ public class WidgetsPage extends HomePage {
         scrollToElementJS(progressBarMenuItem);
         click(progressBarMenuItem);
         return new ProgressBarPage();
+    }
+
+    public SliderPage clickSlider() {
+        scrollToElementJS(sliderMenuItem);
+        click(sliderMenuItem);
+        return new SliderPage();
     }
 }
